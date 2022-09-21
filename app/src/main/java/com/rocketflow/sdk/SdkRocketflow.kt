99fd3910613adf295c6dfd6f2a90e4f495e0d79c
sanketkudale-nts <sanket.kudale@neutrinotechlabs.com>
2022-09-21 15:42:42 +0530

    initial commit

---
+++ app/src/main/java/com/rocketflow/sdk/SdkRocketflow.kt
@@ -0,0 +1,18 @@
+package com.rocketflow.sdk
+
+import android.util.Log
+
+class SdkRocketflow {
+
+    init {
+        Log.e("initRocketflow","initRocketflow")
+    }
+
+    fun start(){
+        Log.e("startRocketflow","startRocketflow")
+    }
+
+    fun terminate(){
+        Log.e("closeRocketflow","closeRocketflow")
+    }
+}
